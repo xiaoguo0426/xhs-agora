7f1c0c5b0fb9b3be6cd486dace767c18c6f0ddd5
Peter <peter.guo@prizmcn.com>
2025-07-01 17:29:15 +0800

    update

--- tests/gen_signature.php
+++ tests/gen_signature.php
@@ -11,7 +11,7 @@ $app_key = '';
 $access_token = '';
 
 $nonce = Signature::genRandomStr();
-$timestamp = (int)round(microtime(true) * 1000);
+$timestamp = round(microtime(true) * 1000);
 
 $signature = Signature::build($app_key, $nonce, $timestamp, $access_token);//生成分享签名
 
